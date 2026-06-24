2a30f66f306d9971d231f236a8b87fb969e8fc27
Wijnand van Leeuwen <wvleeuwen@gmail.com>
2026-06-24 00:23:03 -0700

    Add ts, js and php as languages for tree sitter and the lang servers

--- init.lua
+++ init.lua
@@ -701,7 +701,7 @@ do
     --    https://github.com/pmizio/typescript-tools.nvim
     --
     -- But for many setups, the LSP (`ts_ls`) will work just fine
-    -- ts_ls = {},
+    ts_ls = {},
 
     stylua = {}, -- Used to format Lua code
 
@@ -904,7 +904,7 @@ do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
   -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
+  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'typescript', 'javascript', 'php' }
   require('nvim-treesitter').install(parsers)
 
   ---@param buf integer
