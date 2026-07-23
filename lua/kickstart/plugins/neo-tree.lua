6d3f19a1591fcd7038f0480acbc1ad1507e3c5ee
Wijnand van Leeuwen <wvleeuwen@gmail.com>
2026-07-23 15:05:36 -0700

    Properly add neo-tree and show hidden files

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,6 +11,11 @@ vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', 
 
 require('neo-tree').setup {
   filesystem = {
+    filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
+      hide_gitignored = false,
+    },
     window = {
       mappings = {
         ['\\'] = 'close_window',
