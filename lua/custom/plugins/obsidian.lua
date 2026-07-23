9e56c4238c0c05a87790573993db310f07c56deb
Wijnand van Leeuwen <wvleeuwen@gmail.com>
2026-07-23 15:07:02 -0700

    Add basic obsidian config

---
+++ lua/custom/plugins/obsidian.lua
@@ -0,0 +1,15 @@
+vim.pack.add{"https://github.com/epwalsh/obsidian.nvim"}
+vim.pack.add{"https://github.com/nvim-lua/plenary.nvim"}  -- required dependency
+
+require("obsidian").setup({
+
+  workspaces = {
+    { name = "main", path = "~/Documents/vaults/Personal" },
+  },
+
+  ui = {
+    enable = false
+  }
+
+})
+
