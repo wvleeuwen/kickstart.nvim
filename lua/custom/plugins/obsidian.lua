179b7f65f2df18bc298733e0112263b0e116f5d5
Wijnand van Leeuwen <wvleeuwen@gmail.com>
2026-07-28 15:41:51 -0700

    Comment out obisian setup for now Until I figure out how to set that up on a machine-by-machine basis.

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,15 +1,15 @@
 vim.pack.add{"https://github.com/epwalsh/obsidian.nvim"}
 vim.pack.add{"https://github.com/nvim-lua/plenary.nvim"}  -- required dependency
 
-require("obsidian").setup({
+-- require("obsidian").setup({
 
-  workspaces = {
-    { name = "main", path = "~/Documents/vaults/Personal" },
-  },
+  -- workspaces = {
+  --   { name = "main", path = "~/Documents/vaults/Personal" },
+  -- },
 
-  ui = {
-    enable = false
-  }
-
-})
+--   ui = {
+--     enable = false
+--   }
+--
+-- })
 
